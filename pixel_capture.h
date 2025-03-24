155c289fffc92fa2461780fe19944cf722dd42de
LeeOnYu <loy7153@gmail.com>
2025-03-25 02:02:25 +0900

    solve packet overlap

--- pixel_capture.h
+++ pixel_capture.h
@@ -3,10 +3,10 @@
 
 #include <SDL2/SDL.h>
 
-extern unsigned char* pixels;  // 픽셀 데이터를 저장할 전역 배열
+extern unsigned char *pixels;
 
 void allocatePixelMemory(int screen_width, int screen_height);
 void freePixelMemory();
-void capturePixels(SDL_Renderer* renderer, int screen_width, int screen_height);
+void capturePixels(SDL_Renderer *rR, int screen_width, int screen_height);
 
 #endif // PIXEL_CAPTURE_H
